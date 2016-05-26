3b57643b6caa25989d4b1be6c6c5b56c7b9092bd
birdandbees <jingjing.zhu@gmail.com>
2016-05-26 11:40:37 -0500

    set STATUS var

--- partition_by_year_month.sh
+++ partition_by_year_month.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
+STATUS='success'
 file_path=`echo -e "${WORKFLOW.HDFS_INPUT_PATH}" | perl -pe 's/(.*)\/(.*?)_incr/\2/'`
-
 if [[ -z ${file_path} ]]
 then
+    STATUS='fail'
     exit 1
 fi
 source_schema='postgres_avant_basic_us_'
@@ -13,7 +13,6 @@ script_path='/home/bedrock/scripts/nightly_ingestion/'
 script_name='partition_by_year_month.py'
 source_db='postgres'
 target_db='postgres_refined'
-STATUS='success'
 scl enable python27 "${script_path}${script_name} --update --sourceTable ${script_arg}  --targetTable ${file_path} --sourceDB ${source_db} --targetDB ${target_db}"
 if [[ $? != 0 ]]
 then
